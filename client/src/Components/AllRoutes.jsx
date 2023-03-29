53c2696907df9aaebbd02807935733fd10392864
Md Jawed <mdjawedh22@gmail.com>
2023-03-29 23:15:50 +0530

    landing page done

--- client/src/Components/AllRoutes.jsx
+++ client/src/Components/AllRoutes.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from '../Pages/Landing-page/LandingPage';
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+   <>
+   <Routes>
+<Route path="/" element={<LandingPage/>}/>
+   </Routes>
+   </>
   )
 }
 
 export default AllRoutes;
+
+
+// <div>AllRoutes
+// </div>
---
+++ client/src/Pages/Landing-page/LandingPageData/omgDeals.js
@@ -0,0 +1,68 @@
+let omgDeals = [
+    {
+      id: 1,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/4bb149a9-8496-4e5c-b91a-20545debd71a1672990786584-T-Shirts.jpg",
+    },
+    {
+      id: 2,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/76fb0dc3-3006-4f1c-821b-843f147520e11672990786524-Shirts.jpg",
+    },
+    {
+      id: 3,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/f1ec1bf5-c2c1-4bd0-bbe2-66defbdd9ef01672990786502-Jeans.jpg",
+    },
+    {
+      id: 4,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/c6476c3b-e53e-4f32-ad96-cb5de58b46361672990786547-Sports-Shoes.jpg",
+    },
+    {
+      id: 5,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/d4f4d461-99d5-450a-96f7-c8b3b863448e1672990786437-Casual-Shoes.jpg",
+    },
+    {
+      id: 6,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/b414ce0f-0e89-4791-b506-ede21e1e96521672990786566-Sweatshirts.jpg",
+    },
+    {
+      id: 7,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/88ba4f19-0897-4a98-854d-7d448189995e1672990786481-Jackets.jpg",
+    },
+    {
+      id: 8,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/ae6d390f-f839-4ee7-8641-fc72557dab801672990786459-Flip-Flops.jpg",
+    },
+    {
+      id: 9,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/77e96470-ca78-4cab-b8e5-14ed284d2b741672990939116-Kurta-Sets.jpg",
+    },
+    {
+      id: 10,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/92656ad9-3357-426c-9bd3-e57ec62106b01672990939015-Dresses.jpg",
+    },
+    {
+      id: 11,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/e3feddbb-dea8-41dd-8171-f32ead9453671672990939156-Sweaters-_-Sweatshirts.jpg",
+    },
+    {
+      id: 12,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/c427ea48-b8c4-4c7b-92f9-352adb2487e91672990939177-Tops-_-Tees.jpg",
+    },
+    {
+      id: 13,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/7dae801f-3562-467e-92dd-ef40c472eac21672990939137-Kurtas.jpg",
+    },
+    {
+      id: 14,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/1ff577a6-b8f8-480b-9cec-f317676dc6e01672990939077-Jeans.jpg",
+    },
+    {
+      id: 15,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/c31bda53-0b98-46c5-9ebb-51c5874721f41672990939058-Jackets.jpg",
+    },
+    {
+      id: 16,
+      img: "https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2023/1/6/0f155dec-c813-4b69-a0f9-e894b15dc8821672990939097-Kids-Wear.jpg",
+    }
+   
+  ];
+  export default omgDeals;
