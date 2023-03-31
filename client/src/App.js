65e06cfb004d5817591c7b4fa5c5e32d332badfc
deepti1144 <dipz09085@gmail.com>
2023-03-31 22:55:15 +0530

    navbar done successfully

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import { SignUp } from './Pages/SignUp';
 import AllRoutes from './Components/AllRoutes';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <AllRoutes />
     </div>
   );
